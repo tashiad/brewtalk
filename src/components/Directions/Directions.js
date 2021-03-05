8a95cf8e30189dd27811540d895bd4d3d5d1c8a8
tashiad <tashiadavis10@gmail.com>
2021-03-05 10:21:02 -0700

    Render Directions component

--- src/components/Directions/Directions.js
+++ src/components/Directions/Directions.js
@@ -5,6 +5,8 @@ const Directions = () => {
   return (
     <section>
       <h2>Directions Container!</h2>
+      <button>Get Directions</button>
+      <p className="cheers-message">Cheers! You're officially ready to be social.</p>
     </section>
   )
 }
