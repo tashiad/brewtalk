7456040a45f2338f029a36790530c100d921bcac
tashiad <tashiadavis10@gmail.com>
2021-03-06 13:14:17 -0700

    Add Google Maps API search by selected brewery name link

--- src/components/Directions/Directions.js
+++ src/components/Directions/Directions.js
@@ -15,7 +15,12 @@ const Directions = ({ selectedBrewery, dadJoke }) => {
       <div className="section-top">
         <p className="number">3</p>
         <label className="step" htmlFor="directions">Head out:</label>
-        <button className="button-top button-primary" name="directions">Get Directions</button>
+        {!selectedBrewery.selected && <button className="button-top button-primary" name="directions" disabled={true}>Get Directions</button>}
+        {selectedBrewery.selected &&
+          <a href={`https://www.google.com/maps/search/?api=1&query=${selectedBrewery.name}`} target="_blank" rel="noreferrer">
+            <button className="button-top button-primary" name="directions" disabled={false}>Get Directions</button>
+          </a>
+        }
       </div>
       {checkForSelections()}
     </section>
