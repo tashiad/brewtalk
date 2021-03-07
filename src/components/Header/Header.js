c9df439bcefbd21e20212ce4c390fcfd7aed3c02
tashiad <tashiadavis10@gmail.com>
2021-03-07 15:10:40 -0700

    Make h1 link to homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Header.css'
 
 const Header = () => {
   return (
     <header>
-      <h1>BrewTalk</h1>
+      <Link to="/" style={{ textDecoration: 'none' }}>
+        <h1>BrewTalk</h1>
+      </Link>
       <h2>Breweries + dad jokes for the introverted.</h2>
     </header>
   )
